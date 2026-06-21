9f0ead3260f5aac45aeacd9d9be0a28294f17ffa
celtmen <achintoutain.clement@icloud.com>
2026-06-21 20:25:23 +0200

    Add apps.js with app details and links

---
+++ apps.js
@@ -0,0 +1,14 @@
+const apps = [
+  {
+    name: "App 1",
+    description: "Description de l'app 1",
+    image: "app1.png",
+    link: "https://tonlien.com/app1"
+  },
+  {
+    name: "App 2",
+    description: "Description de l'app 2",
+    image: "app2.png",
+    link: "https://tonlien.com/app2"
+  }
+];
