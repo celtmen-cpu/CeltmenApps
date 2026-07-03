9923d218be8c1d1e3524e7b2320e5fa6f9f4f0cb
celtmen <achintoutain.clement@icloud.com>
2026-07-03 21:03:58 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const apps = [
+  {
+    name: "Calculatrice",
+    image: "calc.png",
+    description: "Une calculatrice simple",
+    link: "calc.html"
+  },
+  {
+    name: "Notes",
+    image: "notes.png",
+    description: "Prendre des notes rapidement",
+    link: "notes.html"
+  },
+  {
+    name: "Météo",
+    image: "meteo.png",
+    description: "Voir la météo",
+    link: "meteo.html"
+  }
+];
