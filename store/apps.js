b1e80405def53eb844b7de0e85c92e7aeb8b3504
celtmen <achintoutain.clement@icloud.com>
2026-07-07 12:56:35 +0200

    Update apps.js

--- store/apps.js
+++ store/apps.js
@@ -1,14 +1,65 @@
 const apps = [
-  {
+
+{
+    id: "night-rush",
+
     name: "Night Rush",
-    description: "Un jeu incroyable",
-    image: "https://celtmen-cpu.github.io/Night-Rush/assets/logoNightRush2.PNG",
-    link: "https://celtmen-cpu.github.io/Night-Rush/assets/logoNightRush2.PNG"
-  },
-  {
-    name: "HavoK",
-    description: "une version fan de l'app de havok faite par alvindev",
-    image: "app2.png",
-    link: "https://tonlien.com/app2"
-  }
+
+    subtitle: "Jeu d'action rapide",
+
+    developer: "Celtmen",
+
+    version: "1.0",
+
+    size: "82 Mo",
+
+    age: "12+",
+
+    description:
+    "Night Rush est un jeu d'action où vous devez survivre et progresser dans un univers rapide et intense.",
+
+    image:
+    "https://celtmen-cpu.github.io/Night-Rush/assets/logoNightRush2.PNG",
+
+    screenshots: [
+        "https://celtmen-cpu.github.io/Night-Rush/assets/screenshot1.png",
+        "https://celtmen-cpu.github.io/Night-Rush/assets/screenshot2.png",
+        "https://celtmen-cpu.github.io/Night-Rush/assets/screenshot3.png"
+    ],
+
+    link:
+    "https://celtmen-cpu.github.io/Night-Rush/"
+},
+
+
+{
+
+    id:"havok",
+
+    name:"HavoK",
+
+    subtitle:"Application fan",
+
+    developer:"AlvinDev",
+
+    version:"1.0",
+
+    size:"25 Mo",
+
+    age:"4+",
+
+    description:
+    "Une version fan de l'application HavoK créée par AlvinDev.",
+
+    image:"app2.png",
+
+    screenshots:[
+        "app2.png",
+        "app2.png"
+    ],
+
+    link:"#"
+
+}
+
 ];
